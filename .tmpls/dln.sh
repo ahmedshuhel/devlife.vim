04989da928800517853f445b017f168f67240668
Shuhel Ahmed <shuhel@ymail.com>
2021-04-10 12:29:31 +0600

    Add templates

---
+++ .tmpls/dln.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+DATE=$(date '+%Y-%m-%d')
+YESTERDAY=$(date -d "yesterday" '+%Y-%m-%d')
+TOMORROW=$(date -d "tomorrow" '+%Y-%m-%d')
+cat  << EOF
+# $DATE
+
+- [Previous]($1)
+
+## Tasks
+EOF
---
+++ .tmpls/dlo.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+DATE=$(date '+%Y-%m-%d')
+
+cat << EOF
+# $DATE
+
+- [Previous]($1)
+
+## Agenda
+
+
+## Decision/Actions
+
+TBD
+EOF
